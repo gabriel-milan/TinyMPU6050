0caf748902ebe3a510ecef5dbb13cec44560c1ba
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2019-03-20 13:27:38 -0300

    Fixed examples

--- examples/PlatformIO_All_Gets_Example/src/main.cpp
+++ examples/PlatformIO_All_Gets_Example/src/main.cpp
@@ -30,76 +30,76 @@ void PrintGets () {
   // Shows raw data
   Serial.println("--- Raw data:");
   Serial.print("Raw AccX = ");
-  Serial.println(GetRawAccX());
+  Serial.println(mpu.GetRawAccX());
   Serial.print("Raw AccY = ");
-  Serial.println(GetRawAccY());
+  Serial.println(mpu.GetRawAccY());
   Serial.print("Raw AccZ = ");
-  Serial.println(GetRawAccZ());
+  Serial.println(mpu.GetRawAccZ());
   Serial.print("Raw GyroX = ");
-  Serial.println(GetRawGyroX());
+  Serial.println(mpu.GetRawGyroX());
   Serial.print("Raw GyroY = ");
-  Serial.println(GetRawGyroY());
+  Serial.println(mpu.GetRawGyroY());
   Serial.print("Raw GyroZ = ");
-  Serial.println(GetRawGyroZ());
+  Serial.println(mpu.GetRawGyroZ());
   // Show readable data
   Serial.println("--- Readable data:");
   Serial.print("AccX = ");
-  Serial.print(GetAccX());
+  Serial.print(mpu.GetAccX());
   Serial.println(" m/s²");
   Serial.print("AccY = ");
-  Serial.print(GetAccY());
+  Serial.print(mpu.GetAccY());
   Serial.println(" m/s²");
   Serial.print("AccZ = ");
-  Serial.print(GetAccZ());
+  Serial.print(mpu.GetAccZ());
   Serial.println(" m/s²");
   Serial.print("GyroX = ");
-  Serial.print(GetGyroX());
+  Serial.print(mpu.GetGyroX());
   Serial.println(" degrees/second");
   Serial.print("GyroY = ");
-  Serial.print(GetGyroY());
+  Serial.print(mpu.GetGyroY());
   Serial.println(" degrees/second");
   Serial.print("GyroZ = ");
-  Serial.print(GetGyroZ());
+  Serial.print(mpu.GetGyroZ());
   Serial.println(" degrees/second");
   // Show angles based on accelerometer only
   Serial.println("--- Accel angles:");
   Serial.print("AccelAngX = ");
-  Serial.println(GetAngAccX());
+  Serial.println(mpu.GetAngAccX());
   Serial.print("AccelAngY = ");
-  Serial.println(GetAngAccY());
+  Serial.println(mpu.GetAngAccY());
   Serial.print("AccelAngZ = ");
-  Serial.println(GetAngAccZ());
+  Serial.println(mpu.GetAngAccZ());
   // Show angles based on gyroscope only
   Serial.println("--- Gyro angles:");
   Serial.print("GyroAngX = ");
-  Serial.println(GetAngGyroX());
+  Serial.println(mpu.GetAngGyroX());
   Serial.print("GyroAngY = ");
-  Serial.println(GetAngGyroY());
+  Serial.println(mpu.GetAngGyroY());
   Serial.print("GyroAngZ = ");
-  Serial.println(GetAngGyroZ());
+  Serial.println(mpu.GetAngGyroZ());
   // Show angles based on both gyroscope and accelerometer
   Serial.println("--- Filtered angles:");
   Serial.print("FilteredAngX = ");
-  Serial.println(GetAngX());
+  Serial.println(mpu.GetAngX());
   Serial.print("FilteredAngY = ");
-  Serial.println(GetAngY());
+  Serial.println(mpu.GetAngY());
   Serial.print("FilteredAngZ = ");
-  Serial.println(GetAngZ());
+  Serial.println(mpu.GetAngZ());
   // Show filter coefficients
   Serial.println("--- Angle filter coefficients:");
   Serial.print("Accelerometer percentage = ");
-  Serial.print(GetFilterAccCoeff());
+  Serial.print(mpu.GetFilterAccCoeff());
   Serial.println('%');
   Serial.print("Gyroscope percentage = ");
-  Serial.print(GetFilterGyroCoeff());
+  Serial.print(mpu.GetFilterGyroCoeff());
   Serial.println('%');
   // Show accel/gyro deadzones
   Serial.println("--- Deadzone:");
   Serial.print("Accelerometer deadzone = ");
-  Serial.print(GetAccelDeadzone());
+  Serial.print(mpu.GetAccelDeadzone());
   Serial.println(" m/s²");
   Serial.print("Gyroscope deadzone = ");
-  Serial.print(GetGyroDeadzone());
+  Serial.print(mpu.GetGyroDeadzone());
   Serial.println(" degrees/second");
 }
 
